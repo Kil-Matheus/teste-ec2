f1402bbb65c00731e89d08235142507681412345
Kil Matheus <kil.teixeira@sou.inteli.edu.br>
2023-09-28 20:14:08 -0300

    feat: teste

--- back/app.py
+++ back/app.py
@@ -1,20 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/somar', methods=['POST'])
+@app.route('/sua-rota-api', methods=['GET'])
 def somar():
     try:
-        valor1 = float(request.form['valor1'])
-        valor2 = float(request.form['valor2'])
-        resultado = valor1 + valor2
+        numero1 = float(request.args.get('numero1', 0))  # Use 0 como valor padrão se não for fornecido
+        numero2 = float(request.args.get('numero2', 0))  # Use 0 como valor padrão se não for fornecido
+        resultado = numero1 + numero2
+
         return jsonify({'resultado': resultado})
     except Exception as e:
-        return jsonify({'error': 'Erro ao somar valores'})
+        return jsonify({'erro': 'Erro na operação de soma'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80)
